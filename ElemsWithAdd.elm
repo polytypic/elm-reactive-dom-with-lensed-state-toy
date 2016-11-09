a03a7421eb0873dca864f9a028fd5ac4f0e7dc2f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-11-09 22:08:54 +0200

    Oops

---
+++ ElemsWithAdd.elm
@@ -0,0 +1,12 @@
+module ElemsWithAdd exposing (..)
+
+import Array exposing (Array)
+import Dom exposing (..)
+import Lens exposing (..)
+import Elems
+
+elemsWithAdd : (Lens m (Maybe a) -> a -> Html m) -> a -> Lens m (Array a) -> Html m
+elemsWithAdd elem new values =
+  div []
+   [ button [onClick (Lens.modify values (Array.push new))] [text "New"]
+    , Elems.elems elem values ]
