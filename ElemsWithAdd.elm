df8c8ce96d1aabfb444aca8ff6881db544b8d907
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-11-10 08:25:56 +0200

    More explicit

--- ElemsWithAdd.elm
+++ ElemsWithAdd.elm
@@ -2,12 +2,12 @@ module ElemsWithAdd exposing (..)
 
 import Array exposing (Array)
 import Dom exposing (..)
-import Lens exposing (..)
-import Elems
+import Lens exposing (Lens)
+import Elems exposing (elems)
 
 elemsWithAdd : (Lens m (Maybe a) -> a -> Html m) -> a -> Lens m (Array a) -> Html m
 elemsWithAdd elem new values =
   div []
    [ button [onClick (Lens.modify values (Array.push new))] [text "New"]
-   , Elems.elems elem values
+   , elems elem values
    ]
